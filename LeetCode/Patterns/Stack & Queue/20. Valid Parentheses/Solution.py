e0b40cbc2343cc9d855d95299a8c321421be18db
Dheeraj kannekanti <kcrao.g1259@gmail.com>
2026-08-21 15:20:43 +0530

    Add Python Solution to Patterns/Stack & Queue

---
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for ch in s:
+            if ch in '({[':
+                stack.append(ch)
+            else:
+                if (stack[-1]=='('and ch==')') or (stack[-1]=='{'and ch=='}') or (stack[-1]=='[' and ch==']'):
+                    stack.pop()
+        return len(stack)==0 
+
+
+        
