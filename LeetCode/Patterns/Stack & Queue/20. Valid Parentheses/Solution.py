075e98804ad1889be5fa6435746884754078d4ae
Dheeraj kannekanti <kcrao.g1259@gmail.com>
2026-08-21 15:21:07 +0530

    Add Python Solution to Patterns/Stack & Queue

--- LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.py
+++ LeetCode/Patterns/Stack & Queue/20. Valid Parentheses/Solution.py
@@ -5,6 +5,7 @@ class Solution:
             if ch in '({[':
                 stack.append(ch)
             else:
+                if
                 if (stack[-1]=='('and ch==')') or (stack[-1]=='{'and ch=='}') or (stack[-1]=='[' and ch==']'):
                     stack.pop()
         return len(stack)==0 
