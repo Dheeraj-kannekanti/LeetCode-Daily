884039a609d74987047d61de50363283be568183
Dheeraj kannekanti <kcrao.g1259@gmail.com>
2026-08-26 11:11:31 +0530

    Refactor goodNodes and DFS implementation
    Refactor goodNodes method to use local count variable and simplify DFS logic.

--- LeetCode/Python/Medium/1448. Count Good Nodes in Binary Tree/Solution.py
+++ LeetCode/Python/Medium/1448. Count Good Nodes in Binary Tree/Solution.py
@@ -6,21 +6,21 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        self.count = 0
-        return self.dfs(root , root.val)
-    
-        
-        
-    def dfs(self,node,max_so_far):
+        def dfs(self,node,max_so_far):
         if node ==None:
             return 0
-        max_so_far = float('-inf')
         if node.val >= max_so_far:
-            self.count+=1
-            self.max_so_far = node.val
-        self.dfs(node.left , max_so_far)
-        self.dfs(node.right , max_so_far)
-        return self.count
+            count= 1
+        else:
+            count = 0
+        max_so_far = max(max_so_far , node.val)
+        count+=self.dfs(node.left , max_so_far)
+        count+=self.dfs(node.right , max_so_far)
+        return count
+    return self.dfs(root , root.val)
+    
+        
+
 
 
         
