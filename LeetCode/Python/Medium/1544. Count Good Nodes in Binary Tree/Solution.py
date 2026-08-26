2daac92cf840899795fe9b85fab4e4e69bc9a833
Dheeraj kannekanti <kcrao.g1259@gmail.com>
2026-08-26 10:54:13 +0530

    Add Medium Python Solution for 1544. Count Good Nodes in Binary Tree from LeetCode

---
+++ LeetCode/Python/Medium/1544. Count Good Nodes in Binary Tree/Solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.count = 0
+        return self.dfs(root , root.val)
+    
+        
+        
+    def dfs(self,node,max_so_far):
+        if node ==None:
+            return 0
+        max_so_far = ('-inf')
+        if node.val >= max_so_far:
+            self.count+=1
+            self.max_so_far = node.val
+        self.dfs(node.left , max_so_far)
+        self.dfs(node.right , max_so_far)
+        return self.count
+
+
+        
